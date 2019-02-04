c80f55ac3a515596f6d18a504d8d106ebd34ea9a
Sylvester Joosten <sylvester.joosten@gmail.com>
2019-02-04 18:51:23 -0500

    tweaked environment setup

--- zsh/zsh-custom-jlab.sh
+++ zsh/zsh-custom-jlab.sh
@@ -1,10 +1,4 @@
 # ------------------------------------------------------------------------------
-# PATH
-# ------------------------------------------------------------------------------
-# Add commonly used folders to $PATH
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-
-# ------------------------------------------------------------------------------
 # FUNCTIONS
 # ------------------------------------------------------------------------------
 # File search functions
@@ -15,16 +9,8 @@ function r() { grep "$1" ${@:2} -R . }
 function mkcd() { mkdir -p "$@" && cd "$_"; }
 
 # ------------------------------------------------------------------------------
-# ENVIRONMENT MODULES
+# ENVIRONMENT MODULES --> in .zshrc directly so oh-my-zsh can find tmux
 # ------------------------------------------------------------------------------
-if [ -f /etc/profile.d/module.sh ]; then
-  source /etc/profile.d/modules.sh
-
-  if [ -f /group/jpsi-007/local/setup.sh ]; then
-    source /group/jpsi-007/local/setup.sh
-    source /group/jpsi-007/local/pro.sh
-  fi
-fi
 
 ## The root module will also load the correct version of python etc
 #module load root
