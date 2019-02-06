dfb2f56d2f62ef07763ce2fbe15403daf2e25577
Sylvester Joosten <sylvester.joosten@gmail.com>
2019-02-06 11:40:10 -0500

    updated zsh ls colors

--- zsh/launch-zsh-cdaq.sh
+++ zsh/launch-zsh-cdaq.sh
@@ -5,6 +5,9 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 # CDAQ: also use extra bin directories
 export PATH="$HOME/bin:$PATH"
 
+# reset LS_COLORS so we actually use the ones from our zsh theme
+unset LS_COLORS
+
 source /group/jpsi-007/local/setup.sh
 module load zsh
 ZDOTDIR=$HOME/sjjooste/.dotfiles zsh
