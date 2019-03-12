f56ffbf223be59847785e6bf9c2fd293e75fd9ea
Sylvester Joosten <sjoosten@anl.gov>
2019-03-12 15:49:32 -0500

    updated rootlogon.C

--- root/rootlogon.C
+++ root/rootlogon.C
@@ -1,4 +1,9 @@
 {
   gROOT->ProcessLine(".L ~/.rootstyle.C");
+  gSystem->Load("libTree");
+  gSystem->Load("libTreePlayer");
+  gSystem->Load("libHist");
+  gSystem->Load("libGenVector");
+  gSystem->SetBuildDir("$HOME/.root_build_dir");
   set_style();
 }
