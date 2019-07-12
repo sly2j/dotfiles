0b0bddede3b652ad7563265808e04878d9108808
Sylvester Joosten <sjoosten@anl.gov>
2019-07-12 10:40:23 -0400

    updated vim setup

--- root/rootlogon-macos.C
+++ root/rootlogon-macos.C
@@ -2,7 +2,7 @@
   gROOT->ProcessLine(".L ~/.rootstyle.C");
   gROOT->ProcessLine(".include /Users/sjoosten/.local/opt/json_3.6.1/include");
   gROOT->ProcessLine(".include /Users/sjoosten/.local/opt/fmt_5.3.0/include");
-  R__LOAD_LIBRARY("/Users/sjoosten/.local/opt/fmt_5.3.0/lib/libfmt");
+  R__LOAD_LIBRARY(/Users/sjoosten/.local/opt/fmt_5.3.0/lib/libfmt.dylib);
 
   gSystem->Load("libTree");
   gSystem->Load("libTreePlayer");
