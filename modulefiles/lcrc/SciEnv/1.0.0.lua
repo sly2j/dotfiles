60e461d5106395a961b8b50731cb470d83be6541
Sylvester Joosten <sjoosten@anl.gov>
2019-07-17 13:06:19 -0500

    setup LCRC config

--- modulefiles/lcrc/SciEnv/1.0.0.lua
+++ modulefiles/lcrc/SciEnv/1.0.0.lua
@@ -1,3 +1,2 @@
-
 prepend_path("PATH", "/home/sly2j/.local/bin")
 load("gcc/7.1.0-4bgguyp","python/3.6.2-3dp4bht", "libxpm", "libxft", "boost/1.70.0", "singularity/3.0.2", "gsl","cmake", "root", "git", "tmux", "htop", "mathematica", "llvm")
