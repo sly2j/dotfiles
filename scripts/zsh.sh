76c73b69da89a2943f8961517060dfbbf1dc787b
Sylvester Joosten <sylvester.joosten@gmail.com>
2019-09-12 18:06:10 -0400

    bugfix and optimizations for jlab

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -20,7 +20,7 @@ ln -s $SOURCE_DIR/zsh/zsh-custom-${MACHINE}.sh $DOTFILES/.zsh-custom
 echo ln -s $SOURCE_DIR/zsh/oh-my-zsh $DOTFILES/.oh-my-zsh
 ln -s $SOURCE_DIR/zsh/oh-my-zsh $DOTFILES/.oh-my-zsh
 
-if [ ${MACHINE} = "cdaq" ] || [ ${MACHINE} = "jlab"]; then
+if [ ${MACHINE} = "cdaq" ] || [ ${MACHINE} = "jlab" ]; then
   echo ln -s $SOURCE_DIR/zsh/launch-zsh-${MACHINE}.sh $DOTFILES/.launch-zsh.sh
   ln -s $SOURCE_DIR/zsh/launch-zsh-${MACHINE}.sh $DOTFILES/.launch-zsh.sh
 fi
