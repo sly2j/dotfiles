6162eb6095e84a9b5a2386d0783096f9fedeb323
Sylvester Joosten <sjoosten@anl.gov>
2019-09-13 13:55:03 -0500

    small bugfixes for sodium

--- zsh/zsh-custom-linux.sh
+++ zsh/zsh-custom-linux.sh
@@ -1,1 +1,8 @@
 # Do nothing
+
+if [ -f /etc/profile.d/modules.sh ]; then
+  source /etc/profile.d/modules.sh
+  if [ -d /usr/local/etc/modulefiles ]; then
+    module use /usr/local/etc/modulefiles
+  fi
+fi
