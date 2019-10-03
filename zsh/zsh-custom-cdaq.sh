d1a9ea8de10304bc4b3810d6989968d6ed6f8326
Cdaq User <cdaq@jlab.org>
2019-10-03 10:56:37 -0400

    updated cdaq zsh launcher

--- zsh/zsh-custom-cdaq.sh
+++ zsh/zsh-custom-cdaq.sh
@@ -22,8 +22,19 @@ function mkcd() { mkdir -p "$@" && cd "$_"; }
 source /etc/profile.d/modules.sh
 
 if [ -f /group/jpsi-007/local/setup.sh ]; then
-  source /group/jpsi-007/local/setup.sh
-  source /group/jpsi-007/local/pro.sh
+  source /etc/profile.d/modules.sh
+  module use /group/c-csv/local/etc/modulefiles
+  module use /group/jpsi-007/local/modulefiles
+  module use $HOME/vcs2019/local/etc/modulefiles
+  module load tmux/latest
+  module load python3/latest
+  module load vim/latest
+  module load git/latest
+  module load ruby/2.5.3
+  module load cmake/latest
+  module load gcc/latest
+  module load clang-format
+  module load parallel
 fi
 
 ## The root module will also load the correct version of python etc
