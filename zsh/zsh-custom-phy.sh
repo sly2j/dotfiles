7fe2c73ba40742029980a24a5a2c42c4c94cd5be
Sylvester Joosten <sjoosten@anl.gov>
2019-10-04 10:24:55 -0500

    added some extra eic0 specific settings

--- zsh/zsh-custom-phy.sh
+++ zsh/zsh-custom-phy.sh
@@ -3,3 +3,18 @@ module use "$HOME/.dotfiles/modulefiles/phy"
 module load zsh
 module load tmux
 module load vim
+
+## extra setup for EIC0
+export GOPATH="/opt/software/local/stow/go_1.13.1"
+if [[ -d ${GOPATH} ]]; then
+  export PATH="${GOPATH}/bin:${PATH}"
+else 
+  unset GOPATH
+fi
+
+export SINGULARITY_PATH="/opt/software/local/stow/singularity_3.4.1"
+if [[ -d ${SINGULARITY_PATH} ]]; then
+  export PATH="${SINGULARITY_PATH}/bin:${PATH}"
+else
+  unset SINGULARITY_PATH
+fi
